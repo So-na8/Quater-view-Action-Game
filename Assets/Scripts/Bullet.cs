a7c329f2e99aae86c4ab9f03250b4389a89a6903
Sona <skarudtlr8@gmail.com>
2024-04-24 11:13:29 +0900

    Quater-view Action Game/

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damage;
+    public bool isMelee;
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Floor")
         {
             Destroy(gameObject, 3f);
         }
-        else if(collision.gameObject.tag == "Wall")
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!isMelee && other.gameObject.tag == "Wall")
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -40,12 +40,14 @@ public class Player : MonoBehaviour
     bool isReload;
     bool isFireReady = true;
     bool isBorder;
+    bool isDamage;
 
     Vector3 moveVec;
     Vector3 dodgeVec;
 
     Rigidbody rigid;
     Animator anim;
+    MeshRenderer[] meshs;
 
     GameObject nearObject;
     Weapon equipWeapon;
@@ -56,6 +58,7 @@ public class Player : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
+        meshs = GetComponentsInChildren<MeshRenderer>();
     }
 
     void Update()
@@ -107,7 +110,7 @@ public class Player : MonoBehaviour
 
     void Turn()
     {
-        // 1. Å°º¸µå¿¡ ÀÇÇÑ È¸Àü
+        // 1. Å°ï¿½ï¿½ï¿½å¿¡ ï¿½ï¿½ï¿½ï¿½ È¸ï¿½ï¿½
         transform.LookAt(transform.position + moveVec);
 
         if(fDown)
@@ -330,6 +333,35 @@ public class Player : MonoBehaviour
                     break;
             }
             Destroy(other.gameObject);
+        }
+        else if(other.tag == "EnemyBullet")
+        {
+            if (!isDamage)
+            {
+                Bullet enemyBullet = other.GetComponent<Bullet>();
+                health -= enemyBullet.damage;
+                
+                if(other.GetComponent<Rigidbody>() != null)
+                    Destroy(other.gameObject);
+                    
+                StartCoroutine(OnDamage());
+            }
+        }
+    }
+
+    IEnumerator OnDamage()
+    {
+        isDamage = true;
+        foreach(MeshRenderer mesh in meshs)
+        {
+            mesh.material.color = Color.yellow;
+        }
+        yield return new WaitForSeconds(1f);
+
+        isDamage = false;
+        foreach (MeshRenderer mesh in meshs)
+        {
+            mesh.material.color = Color.white;
         }
     }
 
